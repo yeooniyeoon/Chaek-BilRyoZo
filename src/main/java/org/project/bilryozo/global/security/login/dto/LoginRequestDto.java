80bf3e54f42c8fe57abf1b331a09dba687be9526
ycyeon <ickite@naver.com>
2024-09-04 23:27:12 +0900

    refactor: 사용자 로그인 정보를 LoginRequestDto로 받아오도록 리팩토링 - LoginRequestDto 구현 - 로그인 정보를 기존 Map으로 받아오던 방식에서 dto로 변경하여 명시적으로 수정

--- src/main/java/org/project/bilryozo/global/security/login/dto/LoginRequestDto.java
+++ src/main/java/org/project/bilryozo/global/security/login/dto/LoginRequestDto.java
@@ -1,4 +1,4 @@
-package org.project.bilryozo.domain.users.dto.request;
+package org.project.bilryozo.global.security.login.dto;
 
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/org/project/bilryozo/global/security/login/filter/CustomJsonUsernamePasswordAuthenticationFilter.java
+++ src/main/java/org/project/bilryozo/global/security/login/filter/CustomJsonUsernamePasswordAuthenticationFilter.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.project.bilryozo.global.security.login.dto.LoginRequestDto;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -14,15 +15,12 @@ import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
 
 @Slf4j
 public class CustomJsonUsernamePasswordAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
     private static final String DEFAULT_LOGIN_REQUEST_URL = "/login"; // "/login"으로 오는 요청을 처리
     private static final String HTTP_METHOD = "POST"; // 로그인 HTTP 메소드는 POST
     private static final String CONTENT_TYPE = "application/json"; // JSON 타입의 데이터로 오는 로그인 요청만 처리
-    private static final String USERNAME_KEY = "username"; // 회원 로그인 시 아이디 요청 JSON Key : "username"
-    private static final String PASSWORD_KEY = "password"; // 회원 로그인 시 비밀번호 요청 JSon Key : "password"
     private static final AntPathRequestMatcher DEFAULT_LOGIN_PATH_REQUEST_MATCHER =
             new AntPathRequestMatcher(DEFAULT_LOGIN_REQUEST_URL, HTTP_METHOD); // "/login" + POST로 온 요청에 매칭된다.
 
@@ -44,9 +42,9 @@ public class CustomJsonUsernamePasswordAuthenticationFilter extends AbstractAuth
                     + request.getContentType());
         }
         String messageBody = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
-        Map<String, String> useranmePasswordMap = objectMapper.readValue(messageBody, Map.class);
-        String username = useranmePasswordMap.get(USERNAME_KEY);
-        String password = useranmePasswordMap.get(PASSWORD_KEY);
+        LoginRequestDto loginRequestDto = objectMapper.readValue(messageBody, LoginRequestDto.class);
+        String username = loginRequestDto.getUsername();
+        String password = loginRequestDto.getPassword();
 
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
 

ef56797fe5d0cf0caf80315194a2b6f693d0899e
ycyeon <ickite@naver.com>
2024-09-10 01:54:12 +0900

    feature: 도서 전체 조회에 페이징, 검색 기능 추가 - 기존 도서 전체 조회 기능에 pageable, type, keyword를 파라미터로 받아 페이징과 검색 기능을 추가

--- src/main/java/org/project/bilryozo/domain/books/repository/BooksRepository.java
+++ src/main/java/org/project/bilryozo/domain/books/repository/BooksRepository.java
@@ -1,10 +1,15 @@
 package org.project.bilryozo.domain.books.repository;
 
 import org.project.bilryozo.domain.books.domain.Books;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BooksRepository extends JpaRepository<Books, Long> {
-
+    Page<Books> findByIsbnContaining(String keyword, PageRequest of);
+    Page<Books> findByTitleContaining(String keyword, PageRequest of);
+    Page<Books> findByAuthorContaining(String keyword, PageRequest of);
+    Page<Books> findByPublisherContaining(String keyword, PageRequest of);
 }
